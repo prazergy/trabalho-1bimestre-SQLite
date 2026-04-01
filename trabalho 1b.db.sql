a1f1266656174e5df699440cd8ed3b0d32b20c73
prazergy <gysellerodrigues2008@gmail.com>
2026-04-01 10:31:04 -0300

    Add files via upload

---
+++ trabalho 1b.db.sql
@@ -0,0 +1,10 @@
+--
+-- Arquivo gerado com SQLiteStudio v3.4.17 em qua abr 1 10:27:18 2026
+--
+-- Codificação de texto usada: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
